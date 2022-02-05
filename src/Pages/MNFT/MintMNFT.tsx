880f02f471fbe87266e707e114b860dbf2b179d6
semgoSE <semgoSE@gmail.com>
2022-02-06 01:11:03 +0300

    preview load image ipfs

--- src/Pages/MNFT/MintMNFT.tsx
+++ src/Pages/MNFT/MintMNFT.tsx
@@ -1,4 +1,4 @@
-import { Button, FilledInput, Grid, Paper, Stack, TextareaAutosize, TextField, Typography } from "@mui/material";
+import { Button, CircularProgress, FilledInput, Grid, Paper, Stack, TextareaAutosize, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { Icon48PictureOutline } from "@vkontakte/icons";
 import { useCallback, useContext, useEffect, useState } from "react";
@@ -11,6 +11,7 @@ import { useDropzone } from 'react-dropzone'
 import { Web3Storage } from 'web3.storage/dist/bundle.esm.min.js'
 
 interface MNFTForm {
+    image_cid?: string,
     image?: string,
     name?: string,
     description?: string,
@@ -22,9 +23,7 @@ interface MNFTForm {
 const MintMNFT = () => {
     const { account, connect } = useContext(AccountContext);
     const [loadingImage, setLoadingImage] = useState(false);
-    const [form, setForm] = useState<MNFTForm>({
-        image: "https://previews.123rf.com/images/rglinsky/rglinsky1201/rglinsky120100188/12336990-%E5%9E%82%E7%9B%B4%E6%8C%87%E5%90%91%E3%81%AE%E3%83%91%E3%83%AA%E3%80%81%E3%83%95%E3%83%A9%E3%83%B3%E3%82%B9%E3%81%A7%E6%9C%89%E5%90%8D%E3%81%AA%E3%82%A8%E3%83%83%E3%83%95%E3%82%A7%E3%83%AB%E5%A1%94%E3%81%AE%E3%82%A4%E3%83%A1%E3%83%BC%E3%82%B8%E3%81%A7%E3%81%99%E3%80%82.jpg"
-    });
+    const [form, setForm] = useState<MNFTForm>({});
 
     function onChange(e: any) {
         const { name, value } = e.target;
@@ -34,11 +33,27 @@ const MintMNFT = () => {
         }));
     }
 
+    function renameFile(originalFile: File, newName: string) {
+        return new File([originalFile], newName, {
+            type: originalFile.type,
+            lastModified: originalFile.lastModified,
+        });
+    }
+
     const onDrop = async (acceptedFiles: any) => {
+        let files: File[] = [];
         setLoadingImage(true);
         const client = new Web3Storage({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDM4OTdjNzAxNzkzNzY5NjMzZmI2NkVDQjE4NkE2OTczMjk4RDFjMzYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MzIzOTk1MzY0ODUsIm5hbWUiOiJ0ZXN0In0.3vRkc5ObyMCWz_aJ5L8IHnm7imjCbYu8InziDoKZquM" })
-        const cid = await client.put(acceptedFiles)
+        acceptedFiles.forEach((file: any, i: number) => {
+            files.push(renameFile(file, String(i)));
+        });
+        const cid = await client.put(files)
         setLoadingImage(false);
+        setForm((prev: MNFTForm) => ({
+            ...prev,
+            image: "ipfs://" + cid + "/0",
+            image_cid: cid
+        }));
         console.log('stored files with cid:', cid);
     }
 
@@ -56,9 +71,8 @@ const MintMNFT = () => {
         <Box>
             <Header />
             <Stack spacing={2} sx={{ height: "100vh" }} direction="row" justifyContent="center" alignItems="center">
-
                 <Paper>
-                    <Box
+                    {!form.image ? <Box
                         sx={{
                             display: "flex",
                             flexDirection: "column",
@@ -70,26 +84,29 @@ const MintMNFT = () => {
                         height={400}
                         {...getRootProps()}
                     >
-                        <input {...getInputProps()} />
-                        <Icon48PictureOutline fill="#636366" width={100} height={100} />
-                        {isDragActive ?
-                            <Typography>Drag/drope File here</Typography> :
-                            <Typography>Drag 'n' drop some files here, or click to select files</Typography>
-                        }
-                    </Box>
-                    {/* <Box
-                        width={400}
-                        height={400}
-                        sx={{
-                            backgroundImage: `url(${form.image})`,
-                            backgroundColor: "background.paper",
-                            backgroundRepeat: "no-repeat",
-                            backgroundPosition: "center",
-                            backgroundSize: "contain",
-                            borderRadius: 8
-                        }}
-                    >
-                    </Box> */}
+                        {loadingImage ? <CircularProgress /> : <>
+                            <input name="images" {...getInputProps()} />
+                            <Icon48PictureOutline fill="#636366" width={100} height={100} />
+                            {isDragActive ?
+                                <Typography>Drag/drope File here</Typography> :
+                                <Typography>Drag 'n' drop some files here, or click to select files</Typography>
+                            }
+                        </>}
+                    </Box> :
+                        <Box
+                            width={400}
+                            height={400}
+                            sx={{
+                                backgroundImage: `url(https://ipfs.io/ipfs/${form.image_cid}/0)`,
+                                backgroundColor: "background.paper",
+                                backgroundRepeat: "no-repeat",
+                                backgroundPosition: "center",
+                                backgroundSize: "contain",
+                                borderRadius: 8
+                            }}
+                        >
+                        </Box>
+                    }
                 </Paper>
 
                 <Box width="30%">
