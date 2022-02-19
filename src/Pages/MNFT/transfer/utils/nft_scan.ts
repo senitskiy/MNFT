492b3ce972837fb4bcc5d42b8314330150320716
unknown <User@User-001.ALKOR.RU>
2022-02-19 22:05:38 +0300

    add TransferMNFT

--- src/Pages/MNFT/transfer/utils/nft_scan.ts
+++ src/Pages/MNFT/transfer/utils/nft_scan.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-function get_users_nfts(address) {
+export function getUsersNfts(address) {
     var urls = []
     axios.get('http://api-staging.rarible.org/v0.1/items/byOwner?owner=ETHEREUM:' + address)
     .then(res => {
       for(var i = 0; i < res['items'].length; i++) {
-        var new_url = res['items'][i]['meta']
-        urls.push(new_url)
+        var newUrl = res['items'][i]['meta']
+        urls.push(newUrl)
       }
 
       return urls
