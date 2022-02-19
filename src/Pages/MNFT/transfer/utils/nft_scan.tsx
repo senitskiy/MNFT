3055018cacf9f3b15932168fb010d15238d5f719
unknown <User@User-001.ALKOR.RU>
2022-02-19 15:20:11 +0300

    add nft parsing

---
+++ src/Pages/MNFT/transfer/utils/nft_scan.tsx
@@ -0,0 +1,8 @@
+const axios = require('axios')
+
+function get_users_nfts(address) {
+    axios.get('pi.rarible.org/v0.1/items/byOwner?owner=' + address)
+  .then(res => {
+    console.log(res.data);
+  });
+}
