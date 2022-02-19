2f8bb940cbacb4446b4e5e208f5300f839e50154
unknown <User@User-001.ALKOR.RU>
2022-02-19 20:56:43 +0300

    add backend for nft meta

--- src/Pages/MNFT/transfer/utils/nft_scan.ts
+++ src/Pages/MNFT/transfer/utils/nft_scan.ts
@@ -1,8 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 function get_users_nfts(address) {
-    axios.get('pi.rarible.org/v0.1/items/byOwner?owner=' + address)
-  .then(res => {
-    console.log(res.data);
-  });
+    var urls = []
+    axios.get('http://api-staging.rarible.org/v0.1/items/byOwner?owner=ETHEREUM:' + address)
+    .then(res => {
+      for(var i = 0; i < res['items'].length; i++) {
+        var new_url = res['items'][i]['meta']
+        urls.push(new_url)
+      }
+
+      return urls
+    });
 }
